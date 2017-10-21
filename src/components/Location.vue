588008af2e6bdc0c0b6c8da10ac5ec0e91b5a334
sbvorpagel <sbvorpagel@gmail.com>
2017-10-21 20:56:04 -0200

    Modificado location.

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -2,7 +2,7 @@
   <div class="location">
     <h1>Localização</h1>
     <h2>Cerimônia</h2>
-    <p>A cerimônia de casamento, irá ocorrer na Igreja Luterana do Brasil, localizada na Rua Rui Barbosa, esquina com a Rua Sete Quedas, em Santa Tereza do Oeste, Paraná, e terá início as 20h.</p>
+    <p>A cerimônia de casamento, irá ocorrer na Igreja Evangélica Luterana do Brasil, localizada na Rua Rui Barbosa, esquina com a Rua Sete Quedas, em Santa Tereza do Oeste, Paraná, e terá início as 20h.</p>
     <div>
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d827.0831498598498!2d-53.609401954629384!3d-25.04775199868012!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjXCsDAyJzUxLjkiUyA1M8KwMzYnMzEuNiJX!5e1!3m2!1sen!2sbr!4v1507401987157" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
     </div>
